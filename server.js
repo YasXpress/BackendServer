aa1dba81a2cab091eb2c84d5f0d6ff18df7ce040
YasXpress <yasxpress584@gmail.com>
2026-03-25 23:02:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import { configDotenv } from "dotenv";
+import dotenv from 'dotenv';
 
-configDotenv.config()
+dotenv.config();
 
 const app = express();
 
@@ -118,7 +118,5 @@ app.get("/", (req,res)=>{
 
 
 
-// SERVER
-app.listen(process.env.PORT || 5000,()=>{
-  console.log("Server running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
